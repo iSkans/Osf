adfaabb89c2ec33044f349cbb6e411538a665d59
Melo <toosho@me.com>
2014-09-22 14:53:30 +0200

    Refactoring All files

--- index.js
+++ index.js
@@ -2,21 +2,66 @@
 //     (c) 2013 Melo, melo@iskans.com
 //     Osf may be freely distributed under the MIT license.
 
-//-- Load all class --//
-var fs = require('fs'),
-    _ = require('underscore'),
-    Osf = {
-        VERSION: '3.0.0'
-    };
+var _ = require('lodash'),
+    fs = require('fs'),
+    path = require('path');
 
-fs.readdirSync(__dirname+'/lib').forEach(function (file) {
-    _.extend(Osf, require(__dirname+'/lib/' + file));
-});
+module.exports = exports = new function () {
+    this.VERSION = '3.0.2';
+    this.init = function (rootPath, publicPath) {
+        var Osf = this;
 
-//-- Export Osf --//
-if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = Osf;
+        //-- Load Osf libraries --//
+        fs.readdirSync(path.join(__dirname, 'lib')).forEach(function (file) {
+            if (file.substr(-3) === '.js') {
+                _.assign(Osf, require(path.join(__dirname, 'lib', file)));
+            }
+        });
+
+        //-- Initialize application --//
+        Osf.App.init();
+
+        //-- Load configuration --//
+        Osf.App.configuration(Osf.Config(rootPath, publicPath));
+
+        //-- Load models --//
+        Osf.App.models();
+
+        //-- Load translation --//
+        Osf.App.i18n();
+
+        //-- Load logger --//
+        Osf.App.log();
+
+        //-- Load database --//
+        Osf.App.mongoose();
+
+        //-- Load oauth client --//
+        Osf.App.oauthClient();
+
+        //-- Load passport --//
+        Osf.App.passport();
+
+        //-- Load ouath server --//
+        Osf.App.oauthServer();
+
+        //-- Load Csrf --//
+        Osf.App.csrf();
+
+        //-- Load public path --//
+        Osf.App.public();
+
+        //-- Load helmet --//
+        Osf.App.helmet();
+
+        //-- Load controllers --//
+        Osf.App.controller();
+
+        //-- Load Error manager --//
+        Osf.App.error();
     }
-    exports = Osf;
-}
+
+    this.run = function () {
+        this.App.run();
+    }
+};
